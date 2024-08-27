813b110f4c95ae99173d7abff4943bdfec8af86e
Nierevarino <nierevarino2006@gmail.com>
2024-08-27 17:37:11 -0500

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,61 @@
 ﻿using System;
  
+namespace JuegoDeAdivinanzas
+ {
+
+    class Juego
+    {
+    private int numeroSecreto;
+    private int intentos;
+    public Juego()
+        {
+            Random random = new Random();
+            numeroSecreto = random.Next(1, 11);
+            intentos = 0;
+        }
+ // Método para comenzar el juego
+    public void Comenzar()
+        {
+            Console.WriteLine("¡Bienvenido al juego de adivinanzas!");
+            bool adivinado = false;
+            while (!adivinado)
+            {
+                Console.Write("Adivina un número entre 1 y 10: ");
+                int numeroUsuario = int.Parse(Console.ReadLine());
+                intentos++;
+ // Evaluación de la adivinanza
+                switch (numeroUsuario)
+                {
+                    case int n when n == numeroSecreto:
+                    Console.WriteLine($"¡Felicidades! Adivinaste el número en {intentos} intentos.");
+                    adivinado = true;
+                    break;
+
+                    case int n when n < numeroSecreto:
+                    Console.WriteLine("El número es mayor.¡Inténtalo de nuevo!");
+                    break;
+
+                    case int n when n > numeroSecreto:
+                    Console.WriteLine("El número es menor.¡Inténtalo de nuevo!");
+                    break;
+ 
+                    default:
+                    Console.WriteLine("Número no válido, por favor ingresa un número entre 1 y 10.");
+                    break;
+                }
+            }
+        }
+    }
+ class Program
+    {
+    static void Main(string[] args)
+        {
+            Juego juego = new Juego();
+            juego.Comenzar();      
+        }
+    } 
+ }
+
 
 namespace JuegoPokemon
 {
@@ -109,3 +165,4 @@ namespace JuegoPokemon
         }
     }
 }
+
